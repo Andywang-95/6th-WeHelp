da8e2eabe9719f015e981525a5546b201694819d
Jiahe <1995andywang@gmail.com>
2024-11-08 02:35:40 +0800

    基本樣式功能完成，新增選單錨點對應，選單懸停樣式，選單scrollbar樣式調整...

--- js/index.js
+++ js/index.js
@@ -1,17 +1,38 @@
 changePadding = () => {
     let headerHeight = document.querySelector(".header").offsetHeight;
 
-    const container_padding_top = document.querySelector('.container');
-    container_padding_top.style.paddingTop = headerHeight + 'px'
+    const container = document.querySelector('.container');
+    const body = document.querySelector('body');
+    const question_list = document.querySelectorAll('.question');
+
+    container.style.paddingTop = headerHeight + 'px';
+    container.style.marginTop = '-' + headerHeight + 'px';
+
+    body.style.marginTop = headerHeight + 'px';
+
+    for(let i = 0; i < question_list.length; i++) {
+        question = question_list[i];
+        question.style.paddingTop = String(Number(headerHeight) + 5) + 'px';
+        question.style.marginTop = '-' + headerHeight + 'px';
+    }
+
 }
+
 changeWidth = () => {
     let outlineWidth = document.getElementsByClassName('outline')[0].offsetWidth;
-    // let containerWidth = document.getElementsByClassName('container')[0].offsetWidth;
 
-    const content_width = document.getElementsByClassName('content')[0];
-    // console.log(String(Number(containerWidth) - Number(outlineWidth)) + 'px')
-    content_width.style.paddingLeft = outlineWidth + 'px';
+    const content = document.getElementsByClassName('content')[0];
+    content.style.paddingLeft = String(Number(outlineWidth) + 30) + 'px';
 }
+
+outlineHeight = () => {
+    let headerHeight = document.querySelector(".header").offsetHeight;
+    let pageHeight = window.innerHeight;
+    
+    const outline = document.querySelector('.outline');
+    outline.style.maxHeight = String((pageHeight - Number(headerHeight)) * 0.93) + 'px';
+}
+
 // window.onload = () =>{
 //     changePadding();
 //     changeWidth();
@@ -19,7 +40,9 @@ changeWidth = () => {
 window.addEventListener('DOMContentLoaded', function(){
     changePadding();
     changeWidth();
+    outlineHeight();
 })
 window.addEventListener('resize', changePadding);
 window.addEventListener('resize', changeWidth);
+window.addEventListener('resize', outlineHeight);
 
